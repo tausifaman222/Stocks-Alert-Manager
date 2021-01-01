383758f6b73133aedebcf1ba6b295a9615e1e79a
tausifaman222 <72369808+tausifaman222@users.noreply.github.com>
2021-01-01 21:41:18 +0530

    feat:

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ currentPrice=document.querySelector("input[name=currentPrice]"),
 checkbtn=document.querySelector("#check"),
 para=document.querySelector("#status");
 checkbtn.addEventListener("click",clickHandler);
-
+var bgcolor=["#4B371A","#598335"];
 function clickHandler(event)
 {
+  if(stockQnty.value==='' || currentPrice.value==='' || buyingPrice.value==="")
+  {
+    alert("Input cannot be empty !");
+  }
+  else if(isNaN(stockQnty.value)|| isNaN (currentPrice.value)|| isNaN(buyingPrice.value)  )
+  {
+    alert("Not a Number Detected !Enter Valid Inputs !!");
+  }
+  else
+  {
    getProfitLoss();
+  }
 }
 function getProfitLoss()
 {
@@ -15,17 +26,20 @@ function getProfitLoss()
     var stck=(Number.parseInt(stockQnty.value));
     var cPrice=(Number.parseFloat(currentPrice.value));
     var total=stck*bPrice;
+    var currentTotal=cPrice*stck;
     
-    if(cPrice>=total)
+    if(currentTotal>=total)
     {
-      var absProfit=cPrice-total;
+      var absProfit=currentTotal-total;
       var centProfit=(absProfit/total)*100; 
-      para.innerHTML=`Absolute Profit =${absProfit}\n%-tage Profit=${centProfit}`;
+      document.querySelector("body").style.background=bgcolor[1];
+      para.innerHTML=`Absolute Profit(INR )=${absProfit}  Profit %=${centProfit}` + "👍";
       }
       else{
-        var absLoss=Math.abs(cPrice-total);
+        var absLoss=Math.abs(currentTotal-total);
         var centLoss=(absLoss/total)*100; 
-        para.innerHTML=`Absolute Loss =${absLoss}\n%-tage Loss=${centLoss}`;
+        document.querySelector("body").style.background=bgcolor[0];
+        para.innerHTML=`Absolute Loss =${absLoss}\n %-tage Loss=${centLoss}` + "👎";
 
       }
 }
